848f5e2d0c0434f9d4a81a17a5798c01b733dd79
Denis Bruno Morais Neves <42912364+denisbrunom@users.noreply.github.com>
2022-09-26 16:34:42 -0300

    adicionando_funções

--- bot_msg.py
+++ bot_msg.py
@@ -15,11 +15,41 @@ app = Client(
 @app.on_message(filters.command('help'))
 async def ajuda(client, message):
     print(message.chat.username, message.text)
-    await message.reply('Olá eu sou o ChamberlinBot, essa é uma mensagem de ajuda!') 
+    await message.reply("""Olá eu sou o ChamberlinBot.
+    Comandos:
+    /select
+    /from
+    """) 
+
+@app.on_message(filters.command('select'))
+async def case(client, message):
+    print(message.chat.username, message.text)
+    await message.reply("""
+    A principal função do ***SELECT*** é consultar/buscar os dados de uma tabela em um banco de dados. O caracter * retorna todas as colunas do banco.
+    Exemplo: 
+ select * from departamento;
+
+DEPART     DNOME        UF
+------ -------------- ---------
+ 10     ACCOUNTING      BH
+ 20     RESEARCH        SP
+ 30     SALES           RS
+ 40     OPERATIONS      BA
+ """ )
+
+@app.on_message(filters.command('from'))
+async def case(client, message):
+    print(message.chat.username, message.text)
+    await message.reply(
+        """
+        
+        """
+        )
+
 
 @app.on_message()
 async def messages(client, message):
     print(message.chat.username, message.text)
-    await message.reply('Desculpe, o comando ' + '"'+ message.text+ '"' + ' não é um comando valido ao ainda não o aprendi' )
+    await message.reply('Desculpe, o comando ' + '"' + message.text + '"' + ' não é um comando valido ao ainda não o aprendi.' )
 
 app.run()
